b3a82902058eac308097cf171f7a5eb231312717
weeeeey <qser155@naver.com>
2022-09-01 18:14:53 +0900

    Initial commit

---
+++ MOMENTOM/app.js
@@ -0,0 +1,2 @@
+const b = document.getElementById('title')
+console.dir(b)
---
+++ MOMENTOM/getElement.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// HTML 요소를 통해서 데이터 따오기
+const a = document.getElementById('title');
+// CSS 선택자를 이용해서 데이터 따오기
+const b = document.querySelector("#title");
+
