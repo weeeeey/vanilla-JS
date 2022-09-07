2415391cc7c859413c26bb20d5c39d2ef44f544e
weeeeey <qser155@naver.com>
2022-09-07 19:36:58 +0900

    JS: Deleting in LocalStorage

--- VanillaChrome/js/todo.js
+++ VanillaChrome/js/todo.js
@@ -15,7 +15,9 @@ function deleteToDO(event){
     // event를 통해 path를 알 수 있음(event를 출력한 후 그 안에서 target을 찾기)
     // target 에는 parentElement 가 있음 ( 타겟은 선택된 것의 html 요소를 보여줌)
     const li = event.target.parentElement; //해당 이벤트(클릭된 버튼)의 정확한 위치, 그곳의 부모
+    toDos = toDos.filter(todo=>todo.id !== parseInt(li.id));
     li.remove();
+    saveToDos();
 }
 
 function paintToDo(newToDo){
@@ -23,27 +25,31 @@ function paintToDo(newToDo){
     // 나중에 선택한 리스트를 선택하기 위해서 button을 추가할거임
     // 한줄에 리스트 쓰고 옆에 버튼 쓸거니 리스트 스트링은 span에 넣을거
     const li = document.createElement("li");
+    li.id = newToDo.id;
     const span = document.createElement("span");
     const button = document.createElement("button");
     toDoList.appendChild(li);
     
-    span.innerText=newToDo;
+    span.innerText=newToDo.text;
     li.appendChild(span);
 
     button.innerText='X';
     li.appendChild(button);
     button.addEventListener('click',deleteToDO);
-
-
 }
 
 function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value="";
-    toDos.push(newToDo);
+        // storage에서 인덱스를 통해 delete 하기 위해 id 값을 부여한다.
+    const newToDoObject ={
+        text:newToDo,
+        id:Date.now(),  // 랜덤값
+    };
+    toDos.push(newToDoObject);
+    paintToDo(newToDoObject);
     saveToDos();
-    paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
@@ -55,3 +61,11 @@ if(savedToDos){ //null 이 아니라면
     parseToDos.forEach(paintToDo);
 }
 
+
+
+// function sexyfilter(item){
+//     return item!==3;
+// }
+// let a = [1,2,3,4,5]
+// a = a.filter(sexyfilter)
+// [1,2,4,5] filter는 트루인 값만 다시 넣어줌
